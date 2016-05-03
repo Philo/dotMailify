c1f42a48fb4c73c06fe5138af056c6f5ebace641
philo <phil@stormid.com>
2016-05-03 16:44:21 +0100

    adjusts prefx setting for default configuration

--- src/dotMailify.Core/Config/DefaultEmailProviderSettings.cs
+++ src/dotMailify.Core/Config/DefaultEmailProviderSettings.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Configuration;
 using System.Runtime.CompilerServices;
+using dotMailify.Core.Abstractions;
 using dotMailify.Core.Abstractions.Config;
 
 namespace dotMailify.Core.Config
@@ -9,18 +10,18 @@ namespace dotMailify.Core.Config
     {
         public virtual bool DisableDelivery => GetFromAppSettings(true);
 
-        protected static string GetFromAppSettings(string @default = null, string prefix = "", [CallerMemberName] string memberName = "")
+        protected static string GetFromAppSettings(string @default = null, string prefix = Constants.Settings.SettingsPrefix, [CallerMemberName] string memberName = "")
         {
             return GetAppSetting(memberName, @default, prefix);
         }
 
-        protected static T GetFromAppSettings<T>(T @default = default(T), string prefix = "", 
+        protected static T GetFromAppSettings<T>(T @default = default(T), string prefix = Constants.Settings.SettingsPrefix, 
             [CallerMemberName] string memberName = "")
         {
             return GetAppSetting(memberName, @default, prefix);
         }
 
-        protected static T GetAppSetting<T>(string key, T @default = default(T), string prefix = "")
+        protected static T GetAppSetting<T>(string key, T @default = default(T), string prefix = Constants.Settings.SettingsPrefix)
         {
             var keyToRead = string.IsNullOrWhiteSpace(prefix) ? key : $"{prefix}{key}";
             var value = ConfigurationManager.AppSettings.Get(keyToRead);
